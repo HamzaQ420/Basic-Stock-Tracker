931dfc20518e16cdb32a6a96f2819ef375ee82ea
HamzaQ420 <hamza.qureshi0420@gmail.com>
2024-08-30 11:03:25 -0500

    Adding Percentage Change From Previous Buy/Sell Price
    Write the ticker of the stock that you own, followed by a colon, then whether you own it (B) or recently sold it (S). Then follow that with a comma and the price you bought/sold it for.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import yfinance as yf; from datetime import datetime; import time; import robin_stocks as robin; import os
+import yfinance as yf; from datetime import datetime; import time; import robin_stocks as robin; import os;
 
-f = open(os.getcwd() + "/data.txt", "r"); temp = f.readlines(); tickers = []
-for x in temp: tickers.append(x.replace("\n", ""))
+f = open(os.getcwd() + "/data.txt", "r"); temp = f.readlines(); tickers = []; bs = []; prices = []; previousPrice = 0
+for x in temp:
+    x = x.split(":"); tickers.append(x[0])
+    x = x[1].split(","); bs.append(x[0]); prices.append(float(x[1].replace("\n", "")))
 
-while True:
+run = True
+
+while run:
     time.sleep(5)
     for y in tickers:
         priceInfo = str(yf.Ticker(y).info).split(","); price = ""
@@ -24,5 +28,8 @@ while True:
                 if len(price) < 6:
                     price += (" " * (6 - len(price)))
                 if len(symbol) < 6: symbol += (" " * (6 - len(symbol)))
-                if priceUpdate: print(symbol, " : ", price, datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+                previousPrice = float(prices[tickers.index(y)]);
+                priceChange = -((previousPrice - float(price)) / float(price)) * 100
+                if priceChange > 0: priceChange = "+" + str(priceChange)
+                if priceUpdate: print(symbol, " : ", price, datetime.now().strftime("%d/%m/%Y %H:%M:%S"), priceChange)
     print()
